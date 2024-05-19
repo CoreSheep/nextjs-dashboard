1634205d2ea34d56ad16e04af34afc13f0ddd74f
CoreSheep <leepaul168@gmail.com>
2024-05-19 21:19:13 +0200

    update

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,11 @@
+// This is how you can create different pages in Next.js: create a new route segment using a folder, and add a page file inside it.
+
+
+/**
+ * By having a special name for page files, Next.js allows you to colocate UI components, test files, and other related code with your routes. 
+ * Only the content inside the page file will be publicly accessible. For example, the /ui and /lib folders are colocated inside the /app folder 
+ * along with your routes.
+ */
+export default function Page() {
+    return <p>Invoices Page</p>;
+  }
